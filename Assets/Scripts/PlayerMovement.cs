df0ce56686d3a812191ee5279c9fb9fcd28f2126
gonzaloramireziti <gonzaloramireziti@gmail.com>
2022-10-08 16:53:26 -0300

    Init

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    
     [SerializeField] float speed;
+    public Rigidbody rigidbody;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();    
-    }
+ 
 
     // Update is called once per frame
     void Update()
